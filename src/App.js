b0cba2c0a5e63430a5f47c1f9910693a842b2758
Bernard Mordan <bernardmordan@gmail.com>
2023-02-28 17:06:11 +0000

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const data = [
     {
@@ -18,57 +18,50 @@ const data = [
     }
 ]
 
-function PersonCard ({color, firstName, catchphrase}) {
+const colors = ["hotpink", "lime", "salmon", "yellow", "midnightblue"]
+
+function PersonCard ({firstName, catchphrase, setBorderIndex}) {
+    const [colorIndex, setColorIndex] = useState(0)
+
+    useEffect(() => {
+        setBorderIndex(colorIndex)
+    }, [colorIndex])
+
     const style = {
         width: '12rem', 
         padding: '1rem', 
-        backgroundColor: color
+        backgroundColor: colors[colorIndex]
     }
+
+    const changeColor = () => {
+        if (colorIndex >= colors.length - 1) {
+            setColorIndex(0)
+        } else {
+            setColorIndex(colorIndex + 1)
+        }
+    }
+
     return (
         <article style={style}>
             <h3>{firstName}</h3>
-            <blockquote>{catchphrase}</blockquote>
+            <blockquote>{catchphrase} {colorIndex}</blockquote>
+            <button onClick={changeColor}>Change up</button>
         </article>
     )
 }
 
-const Greeting = ({size, greeting}) => {
-    const style = {
-        fontSize: `${size}px`,
-        display: 'inline-block'
-    }
-    return (
-        <h2 style={style}>
-            Hello {greeting}
-        </h2>
-    )
-}
-
-const ListHolder = (props) => {
-    return (
-        <fieldset style={{background: 'hotpink', padding: '1rem'}}>
-            <legend>My List</legend>
-            {props.children}
-        </fieldset>
-    )
-}
-
 export const App = () => {
+    const [borderIndex, setBorderIndex] = useState(0)
+
+    const style = {
+        border: `solid 8px ${colors[borderIndex]}`
+    }
+
     return (
-        <fieldset>
-            <legend>Parent</legend>
-            <Greeting size="32" greeting="Clubbers" />
-            <ListHolder>
-                <li>Do the washing up</li>
-                <li>Learn some more react</li>
-                <li>Stop this now</li>
-                <li>
-                    <Greeting size="7" greeting="squeek"/>
-                </li>
-            </ListHolder>
-            <hr />
+        <fieldset style={style}>
+            <legend>People</legend>
             {data.map(person => {
-                return <PersonCard {...person} />
+                return <PersonCard {...person} setBorderIndex={setBorderIndex} />
             })}
         </fieldset>
     )
